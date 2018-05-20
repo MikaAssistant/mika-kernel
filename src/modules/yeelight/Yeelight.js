9f864b3c98647fc84fe4ccd98ae7ebc5ee6e7866
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2018-05-19 23:24:06 -0300

    trocado o mongodb pelo lowdb

--- src/modules/yeelight/Yeelight.js
+++ src/modules/yeelight/Yeelight.js
@@ -4,7 +4,7 @@ const rgb = require('./../../../config/rgbToInteger.json');
 const Device = require('yeelight.js').Device;
 const Yeelight = require('yeelight.js').Yeelight;
 const yeelight = new Yeelight({verbose: true});
-const DeviceModel = require('../../database/models/device');
+const db = require("./../../../config/database");
 
 let YeelightModule = {
     comands: comands,
@@ -16,68 +16,43 @@ let YeelightModule = {
 
     },
     ligar_lampada: function (parameters) {
-        DeviceModel.findOne({
-            name: 'lampada',
-            group: parameters.group
-        }, function (err, d) {
-            if(err) {
-                return;
-            }
-            const device = new Device({
-                id: d.id,
-                address: d.address,
-                port: d.port,
-             });
-        
-             device
-                .powerOn('on').then(() => console.log('done'))
-                .catch((err) => console.log(err));
-            return true;
-        });
+        let d = db.get('devices').find({name: 'lampada', group: parameters.group}).value();
+        const device = new Device({
+            id: d.id,
+            address: d.address,
+            port: d.port,
+         });
+         device
+            .powerOn('on').then(() => console.log('done'))
+            .catch((err) => console.log(err));
+        return true;
     },
     desligar_lampada: function (parameters) {
-        DeviceModel.findOne({
-            name: 'lampada',
-            group: parameters.group
-        }, function (err, d) {
-            if(err) {
-                return;
-            }
-            const device = new Device({
-                id: d.id,
-                address: d.address,
-                port: d.port,
-             });
-        
-             device
-                .powerOn('off').then(() => console.log('done'))
-                .catch((err) => console.log(err));
-            return true;
-        });
+        let d = db.get('devices').find({name: 'lampada', group: parameters.group}).value();
+        const device = new Device({
+            id: d.id,
+            address: d.address,
+            port: d.port,
+         });
+         device
+            .powerOn('off').then(() => console.log('done'))
+            .catch((err) => console.log(err));
+        return true;
     },
     trocar_cor_lampada: function (parameters) {
 
         let cor = rgb[parameters.cor];
 
-        DeviceModel.findOne({
-            name: 'lampada',
-            group: parameters.group
-        }, function(err, d) {
-            if(err) {
-                console.log(err)
-                return;
-            }
-            const device = new Device({
-                id: d.id,
-                address: d.address,
-                port: d.port,
-             });
-        
-            device
-                .setRgb(cor).then(() => console.log('done'))
-                .catch((err) => console.log(err));
-            return true;
-        });
+        let d = db.get('devices').find({name: 'lampada', group: parameters.group}).value();
+        const device = new Device({
+            id: d.id,
+            address: d.address,
+            port: d.port,
+         });
+         device
+            .setRgb(cor).then(() => console.log('done'))
+            .catch((err) => console.log(err));
+        return true;
     }
 };
 
